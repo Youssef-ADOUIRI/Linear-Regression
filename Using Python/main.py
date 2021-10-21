38e0d3a1d3c1ebf7470d9800701ed90e250cd882
Youssef Adouiri Alaoui <86375309+Youssef-ADOUIRI@users.noreply.github.com>
2021-10-21 22:59:56 +0100

    Add files via upload

--- Using Python/main.py
+++ Using Python/main.py
@@ -2,35 +2,68 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("insurance.csv")
-ages = data['age'].values
-charges = (data['charges'].values)/100
-print("Ages size :",ages.size ," \nCharges size :", charges.size)
+data = pd.read_csv("california_housing_train.csv")
+ages = data['total_rooms'].values
+charges = (data['median_house_value'].values)/100
+
+#print("Ages size :",ages.size ," \nCharges size :", charges.size)
+
+DATA_COUNT = 600
+
+XX = ages[:DATA_COUNT]
+YY = charges[:DATA_COUNT]
+
+
+ages = (XX)/1000
+charges = (YY)/1000
+
+plt.plot(XX , YY, 'r*')
+
+
 m = ages.size
-theta1 = 0.1
-theta2 = 0.2
-lr = 0.3
+theta1 = 0.15
+theta2 = 0.25
+lr = 0.001
 th_arr = np.array([[theta1, theta2]]).T
-inputs = np.array([ages]).T
-Y_out = np.array([charges]).T
-Y = np.ones((m , 1))
 
-H = inputs * Y_out
-print("In :")
+print(th_arr)
+print("\n inputs ")
+
+inputs = (np.array([ages]).T)
+
 print(inputs)
-print(H)
+print("\n Y_OUT")
+Y_out = (np.array([charges]).T)
+print(Y_out)
+# print("\n H")
+# #Y = np.ones((m , 1))
+# H = inputs * Y_out
+# print(H)
+print("\n Predict :")
 
-#train the data
-inte = 50
+inte = 600
+
 def predict(X, Th):
     y = np.zeros(shape=(m, 1))
     o1 = np.ones((m, 1))
     xb = np.column_stack((o1, X))
+    # print(xb)
+    # print("\ndot")
     y = np.dot(xb, Th)
     return y
+
+
+
+
 def cost(Y , Y_h):
     err = (Y - Y_h) * (Y - Y_h)
-    return np.sum(err)/(2*m)
+    cc = (np.sum(err))/(2*m)
+    return cc
+
+
+
+#print(cost(predict(ages , th_arr) , Y_out))
+
 def gradient(X,Y ,Th):
     Pr = predict(X , Th)
     err = (Pr - Y)
@@ -38,29 +71,27 @@ def gradient(X,Y ,Th):
     g2 = (np.dot(X.T , err))/m
     return np.concatenate((g1 , g2) , axis= 0)
 
+
+
 for i in range(inte):
-    outp = predict(ages, th_arr)
-    # backprobagation
-    c = cost(outp, Y_out)
-    Grad = gradient(inputs, Y_out, th_arr)
-    th_arr = th_arr - lr * Grad
+    th_arr = th_arr - lr * gradient(inputs , Y_out ,  th_arr)
+    print(th_arr)
+    
+
+xplt = np.linspace(0,15000,1400)
+yplt = th_arr[0] * xplt + th_arr[1]
+plt.plot(xplt,yplt)
+#plt.axis([0, 6, 0, 20])
 
 
-Th0 = th_arr[0, 0]
-Th1 = th_arr[1, 0]
-
-i = 0
-for v in ages:
-    plt.scatter(v , charges[i] )
-    i += 1
 
 
-xplt = np.linspace(0,50,100)
-yplt = Th1*xplt+Th0
-plt.plot(xplt, yplt, '-r', label='y=2x+1')
-plt.title('Lin')
-plt.xlabel('x', color='#1C2833')
-plt.ylabel('y', color='#1C2833')
-plt.legend(loc='upper left')
-plt.grid()
+# xplt = np.linspace(0,50,100)
+# yplt = Th1*xplt+Th0
+# plt.plot(xplt, yplt, '-r', label='y=2x+1')
+# plt.title('Lin')
+# plt.xlabel('x', color='#1C2833')
+# plt.ylabel('y', color='#1C2833')
+# plt.legend(loc='upper left')
+# plt.grid()
 plt.show()
